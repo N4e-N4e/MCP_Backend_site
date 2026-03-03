d2c9f3bb8c0d642b80fa2a5bb7ec04c981d8af4f
Narsim <56876675+N4e-N4e@users.noreply.github.com>
2026-03-03 00:17:17 -0600

    Update render_build.sh

--- render_build.sh
+++ render_build.sh
@@ -10,9 +10,9 @@ if [[ ! -d $STORAGE_DIR/chrome ]]; then
   echo "...Downloading Chrome"
   mkdir -p $STORAGE_DIR/chrome
   cd $STORAGE_DIR/chrome
-  wget https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_114.0.5735.90-1_amd64.deb
-  dpkg -x ./google-chrome-stable_114.0.5735.90-1_amd64.deb $STORAGE_DIR/chrome
-  rm ./google-chrome-stable_114.0.5735.90-1_amd64.deb
+  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+  rm ./google-chrome-stable_current_amd64.deb
   cd $HOME/project/src
 else
   echo "...Using Chrome from cache"
