ed601621cbe3e23df74bcb1eee0ed46dd7be94f3
Narsim <56876675+N4e-N4e@users.noreply.github.com>
2026-03-02 22:55:17 -0600

    Update render_build.sh

--- render_build.sh
+++ render_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Update apt packages and install Chromium along with its required dependencies
-apt-get update && apt-get install -y chromium-browser chromium-chromedriver
+# apt-get update && apt-get install -y chromium-browser chromium-chromedriver
 
 # Python dependencies for the mcp to run
 pip install -r requirements.txt
