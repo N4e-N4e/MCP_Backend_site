09f26efc0c53270fbe56ccf614f2a920469daf49
Narsim <56876675+N4e-N4e@users.noreply.github.com>
2026-03-11 22:38:23 -0500

    Update MCP.py
    Major Fix

--- MCP.py
+++ MCP.py
@@ -105,7 +105,7 @@ def Read_Block(driver, wait, link):
     # 4) Article (main content) if any....
     try:
         paragraphs = container.find_elements(By.XPATH,
-                                             ".//div[contains(@class, 'usa-prose')]//*[self::p or self::li[parent::ol]][not(ancestor::div[contains(@class,'usa-accordion')]) and not(preceding-sibling::h3[@id='related-reports'])] | .//article/p")
+                                             ".//div[contains(@class, 'usa-prose')]//*[self::h2 or self::h3 or self::p or self::li[parent::ol] or self::li[parent::ul]][not(ancestor::div[contains(@class,'usa-accordion')]) and not(preceding-sibling::h3[@id='related-reports']) and not(ancestor::*[preceding-sibling::h3[@id='related-reports']])] | .//article/p")
         if paragraphs:
             content = " ".join([p.text.strip() for p in paragraphs])
             page["Article Body"] = content
@@ -582,6 +582,7 @@ def SOS_search (item: str) -> list:
 if __name__ == "__main__":
 
     mcp.run()
+
 
 
 
